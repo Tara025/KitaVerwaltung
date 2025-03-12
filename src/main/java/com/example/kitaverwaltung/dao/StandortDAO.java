ffbbe731e44b70f5258ee7feb248fab42aa72341
margi night <Example@hotmail.com>
2025-03-12 10:05:20 +0100

    Standort View

--- src/main/java/com/example/kitaverwaltung/dao/StandortDAO.java
+++ src/main/java/com/example/kitaverwaltung/dao/StandortDAO.java
@@ -11,17 +11,19 @@ import java.util.List;
 
 
 public class StandortDAO {
-
+    private static final String VIEW_NAME = "v_standort";
     private static final String TABLE_NAME = "t_standort";
     private static final DatabaseConnection dbConnection = DatabaseConnection.getInstance();
     private static final Gson gson = new Gson();
 
 
 
+
+
     public static List<Standort> getStandorte() {
         List<Standort> standortList = new ArrayList<>();
 
-        String jsonResponse = dbConnection.sendGetRequest(TABLE_NAME);
+        String jsonResponse = dbConnection.sendGetRequest(VIEW_NAME);
         if (jsonResponse != null) {
             Standort[] standortArray = gson.fromJson(jsonResponse, Standort[].class);
             for (Standort standort : standortArray) {
@@ -35,5 +37,17 @@ public class StandortDAO {
     public static boolean addStandort(Standort standort) {
         String json = gson.toJson(standort);
         return dbConnection.sendPostRequest(TABLE_NAME, json);
+    }
+
+    // Bearbeite einen Standort
+    public static boolean editStandort(Standort standort) {
+        String json = gson.toJson(standort);
+        return dbConnection.sendPutRequest(TABLE_NAME, json);
+    }
+
+    // Lösche einen Standort
+
+    public static boolean deleteStandort(int standort_id) {
+        return dbConnection.sendDeleteRequest(TABLE_NAME, standort_id);
     }
 }
